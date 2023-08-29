3dd417599647cbacb16a1dcc3b94ac82a37e688c
Alexey <dubrik90@yandex.by>
2023-08-29 22:56:36 +0300

    add proffile

---
+++ src/widgets/SideBar/ui/SidebarItem/SideBarItem.tsx
@@ -0,0 +1,29 @@
+import { useTranslation } from 'react-i18next';
+import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import MainIcon from 'shared/assets/icons/main-20-20.svg';
+import { SidebarItemType } from 'widgets/Sidebar/model/items';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { memo } from 'react';
+import cls from './SidebarItem.module.scss';
+
+interface SideBarItemProps {
+    item: SidebarItemType,
+    collapsed: boolean
+}
+
+export const SideBarItem = memo(({ item, collapsed }: SideBarItemProps) => {
+    const { t } = useTranslation();
+
+    return (
+        <AppLink
+            theme={AppLinkTheme.SECONDARY}
+            to={item.path}
+            className={classNames(cls.item, { [cls.collapsed]: collapsed }, [])}
+        >
+            <item.Icon className={cls.icon} />
+            <span className={cls.link}>
+                {t(item.text)}
+            </span>
+        </AppLink>
+    );
+});
